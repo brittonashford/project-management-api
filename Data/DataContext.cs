2cb5cac1782e611a78a64da802fef6a3a976fdbb
Bret Kitchell <brittonashford@gmail.com>
2022-11-14 22:39:13 -0600

    configured Entity Framework, ran first migration, created Issues table/DbSet

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace project_management_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 global using project_management_app_api.Models;
+using Microsoft.EntityFrameworkCore;
+using project_management_api.Data;
 using project_management_api.Services.IssueService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
